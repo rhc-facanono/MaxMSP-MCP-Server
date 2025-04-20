6b02bd04f2d17558c99e20723f11257e40558745
Haokun Tian <tianhaokun258@gmail.com>
2025-04-20 01:13:50 +0100

    add mcp resources

--- server.py
+++ server.py
@@ -5,14 +5,28 @@ import os
 import json
 
 # Configuration: IP and port for MaxMSP's [udpreceive] object
-MAX_OSC_HOST = os.environ.get("MAX_OSC_HOST", "127.0.0.1") # Localhost (adjust if Max runs on another machine)
-MAXMSP_UDP_PORT = int(os.environ.get("MAXMSP_UDP_PORT", "5000")) # Must match your [udpreceive] port in Max
+MAX_OSC_HOST = os.environ.get(
+    "MAX_OSC_HOST", "127.0.0.1"
+)  # Localhost (adjust if Max runs on another machine)
+MAXMSP_UDP_PORT = int(
+    os.environ.get("MAXMSP_UDP_PORT", "5000")
+)  # Must match your [udpreceive] port in Max
 # MAX_FEEDBACK_PORT = int(os.environ.get("MAX_FEEDBACK_PORT", "5001"))
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+docs_path = os.path.join(current_dir, "docs.json")
+with open(docs_path, "r") as f:
+    docs = json.load(f)
+flattened_docs = {}
+for obj_list in docs.values():
+    for obj in obj_list:
+        flattened_docs[obj["name"]] = obj
 
 
 class MaxMSPConnection:
     def __init__(self, max_ip, max_port):
         from pythonosc.udp_client import SimpleUDPClient
+
         self.sock = SimpleUDPClient(max_ip, max_port)
 
     def send_command(self, cmd: dict):
@@ -35,7 +49,7 @@ async def server_lifespan(server: FastMCP):
 mcp = FastMCP(
     "MaxMSPMCP",
     description="MaxMSP integration through the Model Context Protocol",
-    lifespan=server_lifespan
+    lifespan=server_lifespan,
 )
 
 
@@ -56,20 +70,13 @@ def add_max_object(
     """
     maxmsp = ctx.request_context.lifespan_context.get("maxmsp")
     assert len(position) == 2, "Position must be a list of two integers."
-    # position = " ".join(str(x) for x in position)
-    # args = " ".join(str(x) for x in args)
-    # maxmsp.send_command(" ".join([
-    #     "add_object",
-    #     position,
-    #     obj_type,
-    #     varname,
-    #     args
-    # ]))
     cmd = {"action": "add_object"}
-    kwargs = {"position":   position, 
-              "obj_type":   obj_type, 
-              "args":       args,
-              "varname":    varname}
+    kwargs = {
+        "position": position,
+        "obj_type": obj_type,
+        "args": args,
+        "varname": varname,
+    }
     cmd.update(kwargs)
     maxmsp.send_command(cmd)
 
@@ -85,7 +92,7 @@ def remove_max_object(
     """
     maxmsp = ctx.request_context.lifespan_context.get("maxmsp")
     cmd = {"action": "remove_object"}
-    kwargs = {"varname":    varname}
+    kwargs = {"varname": varname}
     cmd.update(kwargs)
     maxmsp.send_command(cmd)
 
@@ -106,18 +113,13 @@ def connect_max_objects(
         inlet_idx (int): Inlet index on the destination object.
     """
     maxmsp = ctx.request_context.lifespan_context.get("maxmsp")
-    # maxmsp.send_command(" ".join([
-    #     "connect_objects",
-    #     src_varname,
-    #     str(outlet_idx),
-    #     dst_varname,
-    #     str(inlet_idx)
-    # ]))
     cmd = {"action": "connect_objects"}
-    kwargs = {"src_varname":    src_varname, 
-              "outlet_idx":     outlet_idx, 
-              "dst_varname":    dst_varname,
-              "inlet_idx":      inlet_idx}
+    kwargs = {
+        "src_varname": src_varname,
+        "outlet_idx": outlet_idx,
+        "dst_varname": dst_varname,
+        "inlet_idx": inlet_idx,
+    }
     cmd.update(kwargs)
     maxmsp.send_command(cmd)
 
@@ -138,20 +140,44 @@ def disconnect_max_objects(
         inlet_idx (int): Inlet index on the destination object.
     """
     maxmsp = ctx.request_context.lifespan_context.get("maxmsp")
-    # maxmsp.send_command(" ".join([
-    #     "disconnect_objects",
-    #     src_varname,
-    #     str(outlet_idx),
-    #     dst_varname,
-    #     str(inlet_idx)
-    # ]))
     cmd = {"action": "disconnect_objects"}
-    kwargs = {"src_varname":    src_varname, 
-              "outlet_idx":     outlet_idx, 
-              "dst_varname":    dst_varname,
-              "inlet_idx":      inlet_idx}
+    kwargs = {
+        "src_varname": src_varname,
+        "outlet_idx": outlet_idx,
+        "dst_varname": dst_varname,
+        "inlet_idx": inlet_idx,
+    }
     cmd.update(kwargs)
     maxmsp.send_command(cmd)
+
+
+@mcp.resource("list://objects")
+def list_objects():
+    """Returns a list of all available objects in MaxMSP."""
+    return list(flattened_docs.keys())
+
+
+@mcp.resource("docs://{object_name}")
+def get_object_doc(object_name: str):
+    """Retrieve the official documentation for a given object.
+
+    Use this resource to understand how a specific object works, including its
+    description, inlets, outlets, arguments, methods(messages), and attributes.
+
+    Args:
+        object_name (str): Name of the object to look up.
+
+    Returns:
+        dict: Official documentation details for the specified object.
+    """
+    try:
+        return flattened_docs[object_name]
+    except KeyError:
+        return {
+            "success": False,
+            "error": "Invalid object name",
+            "suggestion": "Make sure the object name is a valid MaxMSP object name.",
+        }
 
 
 if __name__ == "__main__":
